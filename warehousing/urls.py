dfcd96ffbac719327354a7fac376f8c9de017917
RalyukRoman <161949010+RalyukRoman@users.noreply.github.com>
2026-06-19 17:15:23 +0300

    feaat: add urls to transaction

--- warehousing/urls.py
+++ warehousing/urls.py
@@ -5,7 +5,8 @@ from .views import (
     WarehouseCreateView, 
     WarehouseDeleteView,
     InventoryJournalView, 
-    CreateTransactionView,
+    GrainIncomingCreateView,
+    GrainOutgoingCreateView
 )
 
 
@@ -14,5 +15,6 @@ urlpatterns = [
     path('warehouses/add/', WarehouseCreateView.as_view(), name='warehouse_add'),
     path('warehouses/<uuid:pk>/delete/', WarehouseDeleteView.as_view(), name='warehouse_delete'),
     path('warehouses/journal/', InventoryJournalView.as_view(), name='inventory_journal'),
-    path('warehouses/transaction/add/', CreateTransactionView.as_view(), name='transaction_add'),
+    path('warehouse/incoming/', GrainIncomingCreateView.as_view(), name='grain_incoming'),
+    path('warehouse/outgoing/', GrainOutgoingCreateView.as_view(), name='grain_outgoing')
 ]
